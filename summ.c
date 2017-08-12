b4ced73b790fa1af4cbf20e1bfbf50b22ee980b7
seshanraj <30723616+seshanraj@users.noreply.github.com>
2017-08-12 16:45:00 +0530

    Create summ.c

---
+++ summ.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,c;
+    if(i>0)
+    {
+    for(i=0;i<5;i++)
+            scanf("%d",&i);
+    for(i=0;i<5;i++)
+    {
+        c=c+i;
+    }
+    printf("%d",c);
+}
+}
